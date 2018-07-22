42d84e577f48cc59373d31ac6f4976741dd88937
asile gerek <asilegrk@hotmail.com>
2018-07-22 11:04:16 +0300

    23.07.2018/1

---
+++ ikinci gün.py
@@ -0,0 +1,72 @@
+ornek= "bu gun hava cok guzel"
+
+#print(ornek.capitalize())
+#print(ornek.upper())
+#print("bu gun hava durumu:"+ ornek)
+#print("bu gun hava durumu:{}".format(ornek))
+#print("bu gun hava durum u:", ozel_sayim, ornek)
+#print(ozel_sayim == None)
+
+
+#meyveler["elma", "armut", "ananas"]
+
+#print(meyveler.append("kiraz"))
+#print(meyveler)
+#meyveler.insert(1,"avokado")
+#ozeşl_sayilar=[10,2,5,21,16]
+#ozel_sayilar.sort()
+
+
+#varsayilan_not = {"vise": 0, "final": 0}
+#ogrenciler= {
+
+ #   'Sezer Bozkir' : {'vise': 80, "final": 90},     #tek veya çift tırnak olması durumu değiştirmez ama ikisi aynı olsa iyii olur
+  #  'Fatmanur Bilke ':{'vise':60, "final": 100}
+#}
+
+#print(ogrenciler.get('Enes Sahin'))
+ #if ogrenciler.get('Enes Sahin') == None :
+  #      ogrenciler.update({'Enes Sahin': varsayilan_not,
+                         #  'Mustafa Ersoy':varsayilan_not})
+
+    #print(ogrenciler.get("Yasar Celep", varsayilan_not))
+
+    #print(ogrenciler)
+    #print(ogrenciler.items())
+ #for ogrenci, notu in ogrenciler.items():
+#        ogrenciler[ogrenci]['vise'] = 0
+
+ #    print(ogrenciler)
+
+    #print(ogrenciler)
+
+vize_final_notları = {}
+for İ in range(5):
+
+ ogrenci_ismi = input('ogrenci İsmi : ')
+
+vize_notu= int(input('Vize Notu'))
+final_notu = int(input('Final Notu'))
+
+vize_final_notları.update({ ogrenci_ismi: {'vize': vize_notu ,'final': final_notu}})
+
+print(vize_final_notları)
+
+
+
+ #tekrarrr!!!!
+
+#for(int i=0; i<5; i++)
+
+ #   for i in range(0.6.1)
+
+  #  for i in [0,1,2,3,4,5]:
+
+
+
+
+
+
+
+
+
